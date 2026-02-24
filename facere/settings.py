530334958009ee48a90b1071c6f568b94ec2489b
Akhandbhadouria <148524224+Akhandbhadouria@users.noreply.github.com>
2026-02-24 12:56:11 +0530

    removed the deckerfile

--- facere/settings.py
+++ facere/settings.py
@@ -76,22 +76,12 @@ WSGI_APPLICATION = 'facere.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import dj_database_url
-
-if config('DATABASE_URL', default=''):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL'),
-            conn_max_age=600
-        )
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "mydatabase",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
